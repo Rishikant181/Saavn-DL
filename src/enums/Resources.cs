9374f0b4c99de970fc6c43b20b02d92a43d341ce
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-08-02 19:01:56 +0530

    Added enums for different resource types

---
+++ src/enums/Resources.cs
@@ -0,0 +1,10 @@
+namespace Saavn {
+    /// <summary>
+    /// The different types of resources that can be fetched from Saavn.
+    /// </summary>
+    public enum ResourceType {
+        MUSIC,
+        PLAYLIST,
+        MEDIA_URL
+    }
+}
